6642be8bafce55cb8e49d618b21e4c983949d39f
hichammounadi <hicham.hamza.mounadi@gmail.com>
2022-01-16 19:28:41 +0100

    setting up the createJob function tied to a specific user

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 // connect db
 
 const connectDB = require('./db/connect')
-
+const authenticateUser = require('./middleware/authentication')
 // routers
 
 const authRouter = require('./routes/auth')
@@ -22,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
-app.unsubscribe('/api/v1/jobs', jobsRouter)
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,28 +1,33 @@
-const getAllJobs = async(req,res) => {
-    res.send('Get All jobs')
-}
+const Job = require('../models/Job');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, NotFoundError } = require('../errors');
 
+const getAllJobs = async (req, res) => {
+  res.send('Get All jobs');
+};
 
-const getJob = async(req,res)=>{
-    res.send('get job')
-}
+const getJob = async (req, res) => {
+  res.send('get job');
+};
 
-const createJob = async(req,res)=>{
-    res.send('create job')
-}
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+};
 
-const updateJob = async(req,res)=>{
-    res.send('update job')
-}
+const updateJob = async (req, res) => {
+  res.send('update job');
+};
 
-const deleteJob = async(req,res)=>{
-    res.send('delete job')
-}
+const deleteJob = async (req, res) => {
+  res.send('delete job');
+};
 
 module.exports = {
-    getAllJobs,
-    getJob,
-    createJob,
-    updateJob,
-    deleteJob
-}
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
